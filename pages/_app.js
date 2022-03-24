7e12ff728917f822727e796506ebc79c0a1fe252
Visal Perera <xtrzne@gmail.com>
2022-03-24 09:49:12 -0400

    stable - syntax formatting

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
-import { useEffect } from "react";
-import { doc, setDoc, Timestamp } from "firebase/firestore";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, db } from "../firebase";
-import Loading from "../components/Loading";
-import Login from "./login";
-import "../styles/globals.css";
+import { useEffect } from 'react';
+import { doc, setDoc, Timestamp } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebase';
+import Loading from '../components/Loading';
+import Login from './login';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // useAuthState hook monitors user authentication status
   const [user, loading, error] = useAuthState(auth);
+  // useEffect hook updates the firebase DB every time the user
+  // authentication status changes
   useEffect(() => {
     if (user) {
       setDoc(
-        doc(db, "users", user.uid),
+        doc(db, 'users', user.uid),
         {
           email: user.email,
           lastSeen: Timestamp.now(),
--- utils/getRecipientEmail.js
+++ utils/getRecipientEmail.js
@@ -1,3 +1,4 @@
+// Helper function to retrieve the email of recipient
 const getRecipientEmail = (users, currentUser) => {
   const filteredArr = users.filter((user) => user !== currentUser.email);
   return filteredArr[0];
